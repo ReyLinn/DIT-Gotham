30bfcc1a195b34781d7aefe20efdf6389f745863
Lucas Boisbourdin <m.boisbourdin@gmail.com>
2021-01-29 17:32:34 +0100

    Last documents

---
+++ GitMirror.bat
@@ -0,0 +1,8 @@
+git clone --bare https://github.com/ReyLinn/DIT-Gotham.git
+
+cd DIT-Gotham.git
+git push --mirror https://github.com/EpitechMscPro2020/DIT-Gotham-Boisbourdin.git
+
+cd ..
+
+rmdir /s /q DIT-Gotham.git
